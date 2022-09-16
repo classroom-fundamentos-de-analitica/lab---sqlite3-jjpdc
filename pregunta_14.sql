7562000b7e6d315ef34fecb79920d5f14b37598b
Juan Pablo Diaz Cervantes <53982528+jjpdc@users.noreply.github.com>
2022-09-15 21:54:38 -0500

    Update pregunta_14.sql

--- pregunta_14.sql
+++ pregunta_14.sql
@@ -43,9 +43,9 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 -- 
-SELECT K0, avg(c21) --14
-FROM tbl1  
-NATURAL JOIN tbl2 
+SELECT K0, AVG(c21) 
+FROM tbl1 
+JOIN tbl2 
+ON tbl1.K1 = tbl2.K1 
 WHERE c13 > 400 
 GROUP BY K0;
-
