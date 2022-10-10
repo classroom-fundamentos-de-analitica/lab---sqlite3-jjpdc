727a1d9cf4d9218691ed6c912c6889fc5d88682a
Juan Pablo Diaz Cervantes <53982528+jjpdc@users.noreply.github.com>
2022-10-10 15:18:39 -0500

    Update pregunta_11.sql

--- pregunta_11.sql
+++ pregunta_11.sql
@@ -38,4 +38,18 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 -- 
-SELECT strftime('%Y',c14) AS YEAR, COUNT(*) AS CANT FROM tbl1 WHERE strftime('%Y',c14) = '2018';
+SELECT strftime('%Y',c14) AS YEAR, COUNT(*) FROM tbl1 WHERE strftime('%Y',c14) = '2018';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
